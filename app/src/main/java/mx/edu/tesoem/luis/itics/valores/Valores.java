ff0a1cbd60e3a70773802c06e04d59d52915d074
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 10:35:58 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/luis/itics/valores/Valores.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.luis.itics.valores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Valores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valores);
+    }
+}
